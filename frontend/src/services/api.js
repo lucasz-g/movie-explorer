45672f2007eda26cb43fb64009e4f44aa2c12af6
Lucas Eduardo Garcia <evosystem02.ti@wheatonbrasil.com.br>
2026-03-05 15:44:59 -0300

    .env file with credentials

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "d50230af37142e0cb008669c974a01d6";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
